d285da0d540f71acf16ee064d4686a812eaddcae
Kandavel. A <kandavel.anbazhagan@object-frontier.com>
2016-05-04 16:04:28 +0530

    Intial Commit

---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,31 @@
+
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class FactorialTest {
+	
+	@Test(dataProvider = "dpFactorialValue")
+	public void testFactorialValue(int input, int expected) {
+		
+		try {
+			
+			FactorialSolution fs = new FactorialSolution();
+			int actual = fs.fact(input);
+			Assert.assertEquals(actual, expected);
+		} catch(Exception e) {
+			System.out.println("Enter valid number");
+		}
+		
+	}
+	
+	@DataProvider
+	public Object[][] dpFactorialValue() {
+		
+		return new Object[][] {
+			new Object[] { 5 , 120 }
+		};
+	}
+}
---
+++ src/test/java/PrimeTest.java
@@ -0,0 +1,30 @@
+
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class PrimeTest {
+
+	@Test(dataProvider = "dpPrime")
+	public void testPrime(int input) {
+		
+		try {
+			
+			PrimeNumberSolution ps = new PrimeNumberSolution();
+			boolean actual = ps.numberIsPrime(input);
+			Assert.assertEquals(actual, true);
+		} catch(Exception e) {
+			System.out.println("Enter the valid number");
+		}
+	}
+	
+	@DataProvider
+	public Object[][] dpPrime() {
+		return new Object[][] {
+			new Object[] {2}
+		};
+	}
+	
+}
